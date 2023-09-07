c0c26fab1ed6ec1dcf03987bf33b8dbab4aa90ac
chen <chanwei0912@gmail.com>
2023-09-07 12:23:06 +0800

    update: ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,149 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive, ref } from 'vue';
+import axios from 'axios';
+
+let fileInputEl: HTMLInputElement | null = null;
+let scale = 1;
+let currentFile: File | null = null;
+onMounted(() => {
+  fileInputEl = document.getElementById('upload-file') as HTMLInputElement;
+});
+const changeFile = () => {
+  if (fileInputEl?.files?.length) {
+    currentFile = fileInputEl.files[0];
+    fileInputEl!.value = '';
+    let fr = new FileReader();
+    fr.onload = function () {
+      let imgEl: HTMLImageElement | null = document.getElementById('img') as HTMLImageElement;
+      imgEl!.src = fr.result as string;
+
+      var img = new Image();
+      img.onload = function () {
+        let imgDom = document.querySelector('#img');
+        scale = imgDom!.clientWidth / img.width;
+        ocrText(currentFile!);
+      };
+      img.src = fr.result as string;
+    };
+    fr.readAsDataURL(currentFile);
+  }
+};
+
+interface TextItem {
+  content: string;
+  top: string;
+  left: string;
+  height: string;
+  width: string;
+}
+
+const list = reactive<TextItem[]>([]);
+
+const ocrText = (file: File) => {
+  list.splice(0);
+  const formData = new FormData();
+  formData.append('file', file);
+  axios
+    .post(`http://127.0.0.1:8000/ocr/text?time=${+new Date()}`, formData)
+    .then(function (response: any) {
+      if (response.data?.data?.length) {
+        response.data.data.forEach((item: any) => {
+          let textItem: TextItem = {
+            content: item.content,
+            top: item.rect[0][1] * scale + 'px',
+            left: item.rect[0][0] * scale + 'px',
+            height: (item.rect[3][1] - item.rect[0][1]) * scale + 'px',
+            width: item.rect[1][0] * scale - item.rect[0][0] * scale + 'px',
+          };
+          list.push(textItem);
+        });
+        setTimeout(() => {
+          updateTextFontSize();
+        }, 3000);
+      }
+    })
+    .catch(function (error: any) {
+      console.log(error);
+    });
+};
+
+const updateTextFontSize = () => {
+  let listEl: any = document.getElementsByClassName('text-item');
+  for (let index = 0; index < listEl.length; index++) {
+    const domContainer = listEl[index];
+    let domText = domContainer.querySelector('.text');
+    if (domText.clientWidth <= domContainer.clientWidth) {
+      domText.style.transform = 'none';
+    } else {
+      let r = domContainer.clientWidth / domText.clientWidth;
+      domText.style.transform = 'scale(' + r + ')';
+    }
+  }
+};
+
+const currentContent = ref('');
+const selectText = (item: TextItem) => {
+  currentContent.value = item.content;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="img-box flex-1">
+      <div
+        class="text-item"
+        v-for="item in list"
+        @click="selectText(item)"
+        :style="{ top: item.top, left: item.left, width: item.width, height: item.height, lineHeight: item.height }"
+      >
+        <div class="text">{{ item.content }}</div>
+      </div>
+      <img id="img" src="" alt="" srcset="" />
+    </div>
+    <div style="width: 300px; height: 100%; padding: 20px;box-sizing: border-box;">
+      <input type="file" name="" id="upload-file" @change="changeFile" />
+      <textarea name="" id="" cols="30" rows="10" v-model="currentContent" style="margin-top: 32px"></textarea>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+  display: flex;
+  align-items: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.flex-1 {
+  flex: 1;
+  height: 100%;
+  box-sizing: border-box;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.img-box {
+  position: relative;
+  overflow: auto;
+}
+.img-box img {
+  width: 100%;
+  height: auto;
+}
+
+.text-item {
+  cursor: pointer;
+  position: absolute;
+  border: 1px solid red;
+  background-color: #fff;
+  font-size: 12px;
+  opacity: 1;
+  z-index: 99;
+}
+
+.text-item .text {
+  display: inline-block;
+  color: #000;
+  white-space: nowrap;
+  transform-origin: left center;
 }
 </style>
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
 from typing_extensions import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 from paddleocr import PaddleOCR
-ocr = PaddleOCR(use_angle_cls=True, lang="ch",use_gpu=False)
+import time
 
-app = FastAPI()
 MAX_TASK_COUNT = 2
 CURRENT_TASK_COUNT = 0
 
+
+app = FastAPI()
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def resp(message:str="操作成功",data:any=None,code:int=0):
     return {
@@ -29,19 +41,21 @@ def ocr_text(data:any):
     return arr
 
 @app.get("/ping")
-async def root():
+def root():
     return {"message": "ok"}
 
 
 @app.post("/ocr/text")
-async def create_upload_file(
+def create_upload_file(
     file: UploadFile = File(description="A file read as UploadFile"),
 ):
     global CURRENT_TASK_COUNT,MAX_TASK_COUNT
+    print("CURRENT_TASK_COUNT",CURRENT_TASK_COUNT)
     if CURRENT_TASK_COUNT > MAX_TASK_COUNT:
         return resp("已达到最大任务数，请稍后再试",None,100)
     CURRENT_TASK_COUNT += 1
     try:
+        ocr = PaddleOCR(use_angle_cls=True, lang="ch",use_gpu=False)
         contents = file.file.read()
         result = ocr.ocr(contents, cls=True)
         return resp("操作成功", ocr_text(result),0)
